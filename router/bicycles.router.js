c3cbe2d073b92f47795a13cbc3fccebd0068d6da
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-02-14 14:31:18 +0100

    query.param

--- router/bicycles.router.js
+++ router/bicycles.router.js
@@ -5,22 +5,36 @@ const bicyclesRouter = express.Router();
 
 // Configurando routing y controllers
 bicyclesRouter.get("/", (req, res, next) => {
-  return Bicycle.find()
-  .then((bicycle) => {
-    return res.status(200).json(bicycle);
-  })
-  .catch(err => {
-      const error = new Error (err);
+  let filtro = {};
+  if (req.query.tipo) {
+    filtro = { ...filtro, tipo: req.query.tipo };
+  }
+  const menoresA = Number(req.query.menoresA);
+  const mayoresA = Number(req.query.mayoresA);
+  if (!isNaN(menoresA) && !isNaN(mayoresA)) {
+    filtro = { ...filtro, precio: { $lt: menoresA, $gte: mayoresA } };
+  } else if (!isNaN(menoresA)) {
+    filtro = { ...filtro, precio: { $lt: menoresA }}
+  } else if (!isNaN(mayoresA)) {
+    filtro = { ...filtro, precio: { $gte: mayoresA }};
+  }
+
+  return Bicycle.find(filtro)
+    .then((bicycle) => {
+      return res.status(200).json(bicycle);
+    })
+    .catch((err) => {
+      const error = new Error(err);
       error.status = 500;
-      return next(error)
-  });
+      return next(error);
+    });
 });
 
 bicyclesRouter.get("/:id", (req, res, next) => {
   const id = req.params.id;
   return Bicycle.findById(id)
-    .then(bicycle => {
-      if(!bicycle){
+    .then((bicycle) => {
+      if (!bicycle) {
         const error = new Error("Bike not found");
         error.status = 404;
         return next(error);
@@ -32,51 +46,50 @@ bicyclesRouter.get("/:id", (req, res, next) => {
       error.status = 500;
       return next(error);
     });
-})
+});
 
-bicyclesRouter.post('/', (req, res, next) => {
+bicyclesRouter.post("/", (req, res, next) => {
   const newBicycle = new Bicycle({
     nombre: req.body.nombre,
     tipo: req.body.tipo,
     precio: req.body.precio,
-    
-  })
-  return newBicycle.save()
-    .then(bicycle => {
+  });
+  return newBicycle
+    .save()
+    .then((bicycle) => {
       return res.status(200).json(newBicycle);
     })
-    .catch(err => {
+    .catch((err) => {
       const error = new Error(err);
       error.status = 500;
       return next(error);
     });
 });
 
-bicyclesRouter.put('/:id', (req, res, next) => {
+bicyclesRouter.put("/:id", (req, res, next) => {
   const id = req.params.id;
-  return Bicycle.findByIdAndUpdate(id, {$set: req.body}, {new: true})
-    .then(bicycleUpdated => {
+  return Bicycle.findByIdAndUpdate(id, { $set: req.body }, { new: true })
+    .then((bicycleUpdated) => {
       res.status(200).json(bicycleUpdated);
     })
-    .catch(err => {
-      const error = new Error (err);
+    .catch((err) => {
+      const error = new Error(err);
       error.status = 500;
       return next(error);
-    })
-})
+    });
+});
 
-bicyclesRouter.delete('/:id', (req, res, next) => {
+bicyclesRouter.delete("/:id", (req, res, next) => {
   const id = req.params.id;
   return Bicycle.findByIdAndDelete(id)
-   .then(() => {
-    return res.status(200).json(`Bike with id ${id} delete`);
-  })
-  .catch(err => {
-    const error = new Error(err);
-    error.status = 500;
-    return next(error);
-   })
-})
+    .then(() => {
+      return res.status(200).json(`Bike with id ${id} delete`);
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.status = 500;
+      return next(error);
+    });
+});
 
 module.exports = bicyclesRouter;
-
